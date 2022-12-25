4ac85714e093362a6efbba573727a159f6f087b6
rl8384 <radoslaw.lis@dxc.com>
2022-12-25 14:49:25 +0100

    Refactor 25

--- solutions/25.py
+++ solutions/25.py
@@ -1,17 +1,12 @@
 def a(input):
-    s = 0
-    for line in input.splitlines():
-        for index, ch in enumerate(line[::-1]):
-            s += pow(5, index) * {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}[ch]
-
+    val = {"2": "2", "1": "1", "0": "0", "-": "-1", "=": "-2", "3": "=", "4": "-", "5": "0"}
+    s = sum([pow(5, index) * int(val[ch]) for line in input.splitlines() for index, ch in enumerate(line[::-1])])
     output = ""
     rest = 0
     while s != 0 or rest:
         remainder = s % 5 + rest
-        rest = 0
-        if remainder > 2:
-            rest = 1
-        output = {0: "0", 1: "1", 2: "2", 3: "=", 4: "-", 5: "0"}[remainder] + output
+        rest = 0 if remainder <= 2 else 1
+        output = val[str(remainder)] + output
         s //= 5
     return output
 
